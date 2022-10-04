25ac05a6d2a6a468a54dcd50782368fb0a857b74
pgmfernandes <paulag6@gmail.com>
2022-10-03 21:03:51 -0300

    Adjust of output name

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 
-github_repository = os.environ.get("GITHUB_REPOSITORY")
+github_repository = os.environ['GITHUB_REPOSITORY']
 print(github_repository)
 github_repository = "test-" + github_repository
 print(f"::set-output name=dockerfiles::{github_repository}")
