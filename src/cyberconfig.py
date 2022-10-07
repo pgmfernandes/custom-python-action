66e0a9cf0a649fa3f044db3b4f3fdc8a50262b61
pgmfernandes <paulag6@gmail.com>
2022-10-06 22:17:17 -0300

    Testing of remote download of config

---
+++ src/cyberconfig.py
@@ -0,0 +1,27 @@
+import requests
+import urllib3
+
+
+class CyberManager:
+    config_org_name = "pgmfernandes"
+    config_repo_name = "config-files-examples"
+    config_branch_name = "main"
+
+    def __init__(self, github_auth):
+        self.session = requests.Session()
+        self.session.verify = False
+        self.session.headers.update({
+            'Authorization': f"Bearer {github_auth}",
+            "Content-Type": "application/json",
+            'Accept': "application/vnd.github+json"
+        })
+        urllib3.disable_warnings()
+
+    def get_config(self, repo_name):
+        url = f"https://api.github.com/repos/{CyberManager.config_org_name}/{CyberManager.config_repo_name}/contents/{repo_name}.json"
+        response = self.session.get(url)
+        config = None
+        print(response.status_code)
+        if response.status_code == 200:
+            config = response.json()
+        return config
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import os
+from cyberconfig import *
 
+auth = os.environ['GH_TOKEN']
+config_manager = CyberManager(github_auth=auth)
 github_repository = os.environ['GITHUB_REPOSITORY']
+config = config_manager.get_config(repo_name=github_repository)
+print(config)
+
 print(f"\n\n\nGithub_repository{github_repository}")
 github_repository = "test-" + github_repository
 print(f"::set-output name=dockerfiles::{github_repository}")
